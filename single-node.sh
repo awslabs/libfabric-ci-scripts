bdf60f9f5110265bbb76167c2f0f163acb7e27f6
dipti-kothari <diptikothari2@gmail.com>
2019-05-31 00:13:02 -0700

    Update single-node.sh

--- single-node.sh
+++ single-node.sh
@@ -1,44 +1,60 @@
 #!/bin/sh
 
 set +x
+ami_id=slave_$label[0]
+ami_user=slave_$label[1]
+INTERFACE=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
+VPC_ID=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/$INTERFACE/vpc-id)
+security_id=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 describe-security-groups --filter Name=vpc-id,Values=$VPC_ID Name=group-name,Values=$security_group --query "SecurityGroups[*].GroupId" --output=text)
+SERVER_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${!ami_id} --instance-type $instance_type --enable-api-termination --key-name $slave_keypair_name --security-group-id $security_id --subnet-id $subnet_id --placement AvailabilityZone=$availability_zone --query "Instances[*].InstanceId"   --output=text)
 
-# Pulls the libfabric repository and checks out the pull request commit
-echo "==> Building libfabric"
+for i in `seq 1 40`;
+do
+  SERVER_IP=$(aws ec2 describe-instances --instance-ids $SERVER_ID --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text) && break || sleep 5;
+done
 
-cd $WORKSPACE
-git clone https://github.com/dipti-kothari/libfabric
-cd libfabric
-git fetch origin +refs/pull/$PULL_REQUEST_ID/*:refs/remotes/origin/pr/$PULL_REQUEST_ID/*
-git checkout $PULL_REQUEST_REF -b PRBranch
-./autogen.sh
-./configure --prefix=$WORKSPACE/libfabric/install/ \
-				--enable-debug 	\
-				--enable-mrail 	\
-				--enable-tcp 	\
-				--enable-rxm	\
-				--disable-rxd
-make -j 4
-sudo make install
+echo $slave_keypair_private_key > key.pem
+ssh -i key.pem ${!ami_user}@${SERVER_IP} <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
+	# Pulls the libfabric repository and checks out the pull request commit
+	echo "==> Building libfabric"
 
-echo "==> Building fabtests"
-cd $WORKSPACE/libfabric/fabtests
-./autogen.sh
-./configure --with-libfabric=$WORKSPACE/libfabric/install/ \
-		--prefix=$WORKSPACE/fabtests/install/ \
-		--enable-debug
-make -j 4
-sudo make install
+	cd $WORKSPACE
+	git clone https://github.com/dipti-kothari/libfabric
+	cd libfabric
+	git fetch origin +refs/pull/$PULL_REQUEST_ID/*:refs/remotes/origin/pr/$PULL_REQUEST_ID/*
+	git checkout $PULL_REQUEST_REF -b PRBranch
+	./autogen.sh
+	./configure --prefix=$WORKSPACE/libfabric/install/ \
+					--enable-debug 	\
+					--enable-mrail 	\
+					--enable-tcp 	\
+					--enable-rxm	\
+					--disable-rxd
+	make -j 4
+	sudo make install
 
-# Runs all the tests in the fabtests suite while only expanding failed cases
-EXCLUDE=$WORKSPACE/fabtests/install/share/fabtests/test_configs/$PROVIDER/${PROVIDER}.exclude
-if [ -f $EXCLUDE ]; then
-	EXCLUDE="-R -f $EXCLUDE"
-else
-	EXCLUDE=""
-fi
+	echo "==> Building fabtests"
+	cd $WORKSPACE/libfabric/fabtests
+	./autogen.sh
+	./configure --with-libfabric=$WORKSPACE/libfabric/install/ \
+			--prefix=$WORKSPACE/fabtests/install/ \
+			--enable-debug
+	make -j 4
+	sudo make install
 
-echo "==> Running fabtests"
-LD_LIBRARY_PATH=$WORKSPACE/fabtests/install/lib/:$LD_LIBRARY_PATH	\
-BIN_PATH=$WORKSPACE/fabtests/install/bin/ FI_LOG_LEVEL=debug		\
-$WORKSPACE/fabtests/install/bin/runfabtests.sh -v $EXCLUDE		\
-$PROVIDER 127.0.0.1 127.0.0.1
+	# Runs all the tests in the fabtests suite while only expanding failed cases
+	EXCLUDE=$WORKSPACE/fabtests/install/share/fabtests/test_configs/$PROVIDER/${PROVIDER}.exclude
+	if [ -f $EXCLUDE ]; then
+		EXCLUDE="-R -f $EXCLUDE"
+	else
+		EXCLUDE=""
+	fi
+
+	echo "==> Running fabtests"
+	LD_LIBRARY_PATH=$WORKSPACE/fabtests/install/lib/:$LD_LIBRARY_PATH	\
+	BIN_PATH=$WORKSPACE/fabtests/install/bin/ FI_LOG_LEVEL=debug		\
+	$WORKSPACE/fabtests/install/bin/runfabtests.sh -v $EXCLUDE		\
+	$PROVIDER 127.0.0.1 127.0.0.1
+EOF
+AWS_DEFAULT_REGION=us-west-2 aws ec2 terminate-instances --instance-ids $SERVER_ID
+exit $EXIT_CODE
