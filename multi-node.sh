267928bf64b5d1146fd0127964214c27f34b22af
dipti-kothari <diptikothari2@gmail.com>
2019-05-31 15:11:59 -0700

    Update multi-node.sh

--- multi-node.sh
+++ multi-node.sh
@@ -1,41 +1,27 @@
 #!/bin/sh
 
 set +x
+slave_name=slave_$label
+slave_value=${!slave_name}
+ami=($slave_value)
 
-# Uses curl meta-data to retrieve identical information for instance creation
-AMI_ID=$(curl http://169.254.169.254/latest/meta-data/ami-id)
-AVAILABILITY_ZONE=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
-INSTANCE_TYPE=$(curl http://169.254.169.254/latest/meta-data/instance-type)
-SECURITY_GROUPS=$(curl http://169.254.169.254/latest/meta-data/security-groups)
-KEY_NAME=$(curl http://169.254.169.254/latest/meta-data/public-keys/ | sed -e 's,0=,,g')
-CLIENT_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-INTERFACE=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
-SUBNET_ID=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/$INTERFACE/subnet-id)
-VPC_ID=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/$INTERFACE/vpc-id)
-SECURITY_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 describe-security-groups --filter Name=vpc-id,Values=$VPC_ID Name=group-name,Values=$SECURITY_GROUPS --query "SecurityGroups[*].GroupId" --output=text)
+SERVER_ID1=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
 
-# Launches an identical instance and sets ID and IP environment variables for the instance
-echo "==> Launching instance"
-echo $AMI_ID
-echo $AVAILABILITY_ZONE
-echo $INSTANCE_TYPE
-echo $SECURITY_GROUPS
-echo $KEY_NAME
-echo $CLIENT_IP
-echo $INTERFACE
-echo $SUBNET_ID
-echo $VPC_ID
-echo $SECURITY_ID
-VOLUME=$(curl http://169.254.169.254/latest/meta-data/block-device-mapping/root)
-SERVER_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id $AMI_ID --instance-type $INSTANCE_TYPE --enable-api-termination --key-name $KEY_NAME --security-group-id $SECURITY_ID --subnet-id $SUBNET_ID --placement AvailabilityZone=$AVAILABILITY_ZONE --query "Instances[*].InstanceId"   --output=text)
+SERVER_ID2=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
 # Occasionally needs to wait before describe instances may be called
 
 for i in `seq 1 40`;
 do
-  SERVER_IP=$(aws ec2 describe-instances --instance-ids $SERVER_ID --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text) && break || sleep 5;
+  SERVER_IP1=$(aws ec2 describe-instances --instance-ids $SERVER_ID1 --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text) && break || sleep 5;
+  SERVER_IP1=$(aws ec2 describe-instances --instance-ids $SERVER_ID2 --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text) && break || sleep 5;
 done
-echo $SERVER_IP
 
+echo $SERVER_IP1
+echo $SERVER_IP2
+
+aws ec2 wait instance-status-ok --instance-ids $SERVER_ID
+
+ssh -vvv -T -o StrictHostKeyChecking=no ${ami[1]}@$SERVER_IP <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
 # Pulls the libfabric repository and checks out the pull request commit
 echo "==> Building libfabric on first node"
 cd $WORKSPACE
@@ -58,10 +44,6 @@ sudo make install
 # Holds testing every 5 seconds for 40 attempts until the instance is running
 aws ec2 wait instance-status-ok --instance-ids $SERVER_ID
 
-# Adds the IP's to the respective known hosts
-ssh-keyscan -H -t rsa $SERVER_IP  >> ~/.ssh/known_hosts
-echo "dipti testing2"
-sudo cat ~/.ssh/known_hosts
 ssh -vvv -T -o StrictHostKeyChecking=no $USER@$SERVER_IP <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
   ssh-keyscan -H -t rsa $CLIENT_IP  >> ~/.ssh/known_hosts
   echo "==> Building libfabric on second node"
