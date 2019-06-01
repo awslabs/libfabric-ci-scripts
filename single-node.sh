7ee9d5b249f2f492e6238f765f2674ef23975336
dipti-kothari <diptikothari2@gmail.com>
2019-06-01 15:27:39 -0700

    Update single-node.sh

--- single-node.sh
+++ single-node.sh
@@ -21,44 +21,34 @@ aws ec2 wait instance-status-ok --instance-ids ${SERVER_ID}
 
 #SSH into slave EC2 instance
 ssh -o StrictHostKeyChecking=no -vvv -T -i ~/jenkinWork181-slave-keypair.pem ${ami[1]}@${SERVER_IP} <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
-	# Pulls the libfabric repository and checks out the pull request commit
-	echo "==> Building libfabric"
-	cd ${REMOTE_DIR}
-	git clone https://github.com/dipti-kothari/libfabric
-	cd libfabric
-	git fetch origin +refs/pull/$PULL_REQUEST_ID/*:refs/remotes/origin/pr/$PULL_REQUEST_ID/*
-	git checkout $PULL_REQUEST_REF -b PRBranch
-	./autogen.sh
-	./configure --prefix=${REMOTE_DIR}/libfabric/install/ \
-					--enable-debug 	\
-					--enable-mrail 	\
-					--enable-tcp 	\
-					--enable-rxm	\
-					--disable-rxd
-	make -j 4
-	make install
-
-	echo "==> Building fabtests"
-	cd ${REMOTE_DIR}/libfabric/fabtests
-	./autogen.sh
-	./configure --with-libfabric=${REMOTE_DIR}/libfabric/install/ \
-			--prefix=${REMOTE_DIR}/libfabric/fabtests/install/ \
-			--enable-debug
-	make -j 4
-	make install
-
-	# Runs all the tests in the fabtests suite while only expanding failed cases
-	EXCLUDE=${REMOTE_DIR}/libfabric/fabtests/install/share/fabtests/test_configs/${PROVIDER}/${PROVIDER}.exclude
-	echo $EXCLUDE
-	if [ -f ${EXCLUDE} ]; then
-		EXCLUDE="-R -f ${EXCLUDE}"
-	else
-		EXCLUDE=""
-	fi
-
-	echo "==> Running fabtests"
-	cd ${REMOTE_DIR}/libfabric/fabtests/install/bin/
-	LD_LIBRARY_PATH=${REMOTE_DIR}/libfabric/install/lib/:$LD_LIBRARY_PATH BIN_PATH=${REMOTE_DIR}/libfabric/fabtests/install/bin/ FI_LOG_LEVEL=debug ${REMOTE_DIR}/libfabric/fabtests/install/bin/runfabtests.sh -v ${EXCLUDE} ${PROVIDER} 127.0.0.1 127.0.0.1
+  echo "==> Building libfabric on second node"
+  cd ~
+  mkdir -p $WORKSPACE
+  cd $WORKSPACE
+  git clone https://github.com/dipti-kothari/libfabric
+  cd libfabric
+  git fetch origin +refs/pull/$PULL_REQUEST_ID/*:refs/remotes/origin/pr/$PULL_REQUEST_ID/*
+  git checkout $PULL_REQUEST_REF -b PRBranch
+  ./autogen.sh
+  ./configure --prefix=$WORKSPACE/libfabric/install/ --enable-debug --enable-mrail --enable-tcp --enable-rxm --disable-rxd
+  make -j 4
+  make install
+  echo "==> Building fabtests on second node"
+  cd $WORKSPACE/libfabric/fabtests
+  ./autogen.sh
+  ./configure --with-libfabric=$WORKSPACE/libfabric/install/ --prefix=$WORKSPACE/fabtests/install/ --enable-debug
+  make -j 4
+  make install
+  # Runs all tests in the fabtests suite between two nodes while only expanding
+  # failed cases
+  echo "==> Running fabtests between two nodes"
+  EXCLUDE=$WORKSPACE/fabtests/install/share/fabtests/test_configs/$PROVIDER/${PROVIDER}.exclude
+  if [ -f $EXCLUDE ]; then
+  	EXCLUDE="-R -f $EXCLUDE"
+  else
+  	EXCLUDE=""
+  fi
+  LD_LIBRARY_PATH=$WORKSPACE/fabtests/install/lib/:$LD_LIBRARY_PATH BIN_PATH=$WORKSPACE/fabtests/install/bin/ FI_LOG_LEVEL=debug $WORKSPACE/fabtests/install/bin/runfabtests.sh -v $EXCLUDE $PROVIDER $CLIENT_IP $SERVER_IP
 EOF
-#AWS_DEFAULT_REGION=us-west-2 aws ec2 terminate-instances --instance-ids $SERVER_ID
+AWS_DEFAULT_REGION=us-west-2 aws ec2 terminate-instances --instance-ids $SERVER_ID
 exit $EXIT_CODE
