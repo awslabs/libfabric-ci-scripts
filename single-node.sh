ad5ce10bb920d475d6fb810b783ea7c922e34868
dipti-kothari <diptikothari2@gmail.com>
2019-06-01 13:28:20 -0700

    Update single-node.sh

--- single-node.sh
+++ single-node.sh
@@ -7,6 +7,8 @@ ami=($slave_value)
 SERVER_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
 REMOTE_DIR=/home/${ami[1]}
 PATH=/home/${ami[1]}/libfabric/fabtests/install/bin/:${PATH}
+LD_LIBRARY_PATH=/home/${ami[1]}/libfabric/install/lib/:${LD_LIBRARY_PATH}
+BIN_PATH=/home/${ami[1]}/libfabric/fabtests/install/bin/
 
 # Occasionally needs to wait before describe instances may be called
 for i in `seq 1 40`;
@@ -56,9 +58,6 @@ ssh -o StrictHostKeyChecking=no -vvv -T -i ~/jenkinWork181-slave-keypair.pem ${a
 
 	echo "==> Running fabtests"
 	cd ${REMOTE_DIR}/libfabric/fabtests/install/bin/
-	LD_LIBRARY_PATH=/home/${ami[1]}/libfabric/install/lib/:${LD_LIBRARY_PATH}
-	BIN_PATH=/home/${ami[1]}/libfabric/fabtests/install/bin/
-	PATH=/home/${ami[1]}/libfabric/fabtests/install/bin/:${PATH}
 	echo ${LD_LIBRARY_PATH}
 	echo ${BIN_PATH}
 	echo ${PATH}
