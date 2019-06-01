b979902d4601562221d6d1949c64df9f2bd22543
dipti-kothari <diptikothari2@gmail.com>
2019-05-31 20:00:53 -0700

    Update single-node.sh

--- single-node.sh
+++ single-node.sh
@@ -16,17 +16,15 @@ echo ${PROVIDER}
 ssh -o StrictHostKeyChecking=no -vvv -T -i ~/jenkinWork181-slave-keypair.pem ${ami[1]}@${SERVER_IP} <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
 	# Pulls the libfabric repository and checks out the pull request commit
 	echo "==> Building libfabric"
-	echo ${USER}
-	echo ${HOME}
-	echo ${ami[1]}
-	REMOTE_DIR=/home/${ami[1]}
+	cd /home/${ami[1]}
+	REMOTE_DIR='/home/${ami[1]}'
 	echo ${REMOTE_DIR}
 	git clone https://github.com/dipti-kothari/libfabric
 	cd libfabric
 	git fetch origin +refs/pull/$PULL_REQUEST_ID/*:refs/remotes/origin/pr/$PULL_REQUEST_ID/*
 	git checkout $PULL_REQUEST_REF -b PRBranch
 	./autogen.sh
-	./configure --prefix=${REMOTE_DIR}/libfabric/install/ \
+	./configure --prefix=/home/${ami[1]}/libfabric/install/ \
 					--enable-debug 	\
 					--enable-mrail 	\
 					--enable-tcp 	\
@@ -36,16 +34,16 @@ ssh -o StrictHostKeyChecking=no -vvv -T -i ~/jenkinWork181-slave-keypair.pem ${a
 	make install
 
 	echo "==> Building fabtests"
-	cd ${REMOTE_DIR}/libfabric/fabtests
+	cd /home/${ami[1]}/libfabric/fabtests
 	./autogen.sh
-	./configure --with-libfabric=${REMOTE_DIR}/libfabric/install/ \
-			--prefix=${REMOTE_DIR}/libfabric/fabtests/install/ \
+	./configure --with-libfabric=/home/${ami[1]}/libfabric/install/ \
+			--prefix=/home/${ami[1]}/libfabric/fabtests/install/ \
 			--enable-debug
 	make -j 4
 	make install
 
 	# Runs all the tests in the fabtests suite while only expanding failed cases
-	EXCLUDE=${REMOTE_DIR}/fabtests/install/share/fabtests/test_configs/${PROVIDER}/${PROVIDER}.exclude
+	EXCLUDE=/home/${ami[1]}/libfabric/fabtests/install/share/fabtests/test_configs/${PROVIDER}/${PROVIDER}.exclude
 	if [ -f ${EXCLUDE} ]; then
 		EXCLUDE="-R -f ${EXCLUDE}"
 	else
@@ -53,9 +51,9 @@ ssh -o StrictHostKeyChecking=no -vvv -T -i ~/jenkinWork181-slave-keypair.pem ${a
 	fi
 
 	echo "==> Running fabtests"
-	export LD_LIBRARY_PATH=${REMOTE_DIR}/libfabric/install/lib/:${LD_LIBRARY_PATH}	         \
-	export BIN_PATH=${REMOTE_DIR}/libfabric/fabtests/install/bin/ FI_LOG_LEVEL=debug.      \
-	${REMOTE_DIR}/libfabric/fabtests/install/bin/runfabtests.sh -v ${EXCLUDE}		         \
+	export LD_LIBRARY_PATH=/home/${ami[1]}/libfabric/install/lib/:${LD_LIBRARY_PATH}	         \
+	export BIN_PATH=/home/${ami[1]}/libfabric/fabtests/install/bin/ FI_LOG_LEVEL=debug.      \
+	/home/${ami[1]}/libfabric/fabtests/install/bin/runfabtests.sh -v ${EXCLUDE}		         \
 	${PROVIDER} 127.0.0.1 127.0.0.1
 EOF
 #AWS_DEFAULT_REGION=us-west-2 aws ec2 terminate-instances --instance-ids $SERVER_ID
