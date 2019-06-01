2e23111218e4397011b9e4cb4556c97815e61486
dipti-kothari <diptikothari2@gmail.com>
2019-05-31 19:06:29 -0700

    Update multi-node.sh

--- multi-node.sh
+++ multi-node.sh
@@ -5,44 +5,44 @@ slave_name=slave_$label
 slave_value=${!slave_name}
 ami=($slave_value)
 
-SERVER_ID1=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
+CLIENT_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
 
-SERVER_ID2=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
+SERVER_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
 # Occasionally needs to wait before describe instances may be called
 
 for i in `seq 1 40`;
 do
-  SERVER_IP1=$(aws ec2 describe-instances --instance-ids $SERVER_ID1 --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text) && break || sleep 5;
-  SERVER_IP1=$(aws ec2 describe-instances --instance-ids $SERVER_ID2 --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text) && break || sleep 5;
+  CLIENT_IP=$(aws ec2 describe-instances --instance-ids ${CLIENT_ID} --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text) && break || sleep 5;
+  SERVER_IP=$(aws ec2 describe-instances --instance-ids ${SERVER_ID} --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text) && break || sleep 5;
 done
 
-echo $SERVER_IP1
-echo $SERVER_IP2
-
-aws ec2 wait instance-status-ok --instance-ids $SERVER_ID
-
-ssh -vvv -T -o StrictHostKeyChecking=no ${ami[1]}@$SERVER_IP <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
-# Pulls the libfabric repository and checks out the pull request commit
-echo "==> Building libfabric on first node"
-cd $WORKSPACE
-git clone https://github.com/dipti-kothari/libfabric
-cd libfabric
-git fetch origin +refs/pull/$PULL_REQUEST_ID/*:refs/remotes/origin/pr/$PULL_REQUEST_ID/*
-git checkout $PULL_REQUEST_REF -b PRBranch
-./autogen.sh
-./configure --prefix=$WORKSPACE/libfabric/install/ --enable-debug --enable-mrail --enable-tcp --enable-rxm --disable-rxd
-make -j 4
-sudo make install
-
-echo "==> Building fabtests"
-cd $WORKSPACE/libfabric/fabtests
-./autogen.sh
-./configure --with-libfabric=$WORKSPACE/libfabric/install/ --prefix=$WORKSPACE/fabtests/install/ --enable-debug
-make -j 4
-sudo make install
+echo ${CLIENT_IP}
+echo ${SERVER_IP}
 
 # Holds testing every 5 seconds for 40 attempts until the instance is running
 aws ec2 wait instance-status-ok --instance-ids $SERVER_ID
+aws ec2 wait instance-status-ok --instance-ids $CLIENT_ID
+
+ssh -vvv -T -o StrictHostKeyChecking=no ${ami[1]}@$SERVER_IP <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
+# Pulls the libfabric repository and checks out the pull request commit
+  echo "==> Building libfabric on first node"
+  cd ${HOME}
+  git clone https://github.com/dipti-kothari/libfabric
+  cd libfabric
+  git fetch origin +refs/pull/$PULL_REQUEST_ID/*:refs/remotes/origin/pr/$PULL_REQUEST_ID/*
+  git checkout $PULL_REQUEST_REF -b PRBranch
+  ./autogen.sh
+  ./configure --prefix=$WORKSPACE/libfabric/install/ --enable-debug --enable-mrail --enable-tcp --enable-rxm --disable-rxd
+  make -j 4
+  make install
+
+  echo "==> Building fabtests"
+  cd ${HOME}/libfabric/fabtests
+  ./autogen.sh
+  ./configure --with-libfabric=${HOME}/libfabric/install/ --prefix=${HOME}/libfabric/fabtests/install/ --enable-debug
+  make -j 4
+  make install
+EOF
 
 ssh -vvv -T -o StrictHostKeyChecking=no $USER@$SERVER_IP <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
   ssh-keyscan -H -t rsa $CLIENT_IP  >> ~/.ssh/known_hosts
