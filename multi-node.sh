896fffda595983ceb404a03e5cd83fab67781429
dipti-kothari <diptikothari2@gmail.com>
2019-06-01 21:34:19 -0700

    Update multi-node.sh

--- multi-node.sh
+++ multi-node.sh
@@ -8,7 +8,8 @@ REMOTE_DIR=/home/${ami[1]}
 
 CLIENT_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
 SERVER_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId"   --output=text)
-
+echo ${CLIENT_ID}
+echo ${SERVER_ID}
 # Occasionally needs to wait before describe instances may be called
 for i in `seq 1 40`;
 do
