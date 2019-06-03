ac22df14f0cd0f2a893e7710c212b74a3040ccd1
dipti-kothari <diptikothari2@gmail.com>
2019-06-02 21:46:56 -0700

    Update multi-node.sh

--- multi-node.sh
+++ multi-node.sh
@@ -48,7 +48,7 @@ ssh -o SendEnv=REMOTE_DIR -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypa
 EOF
 
 echo "==> Entering second node"
-ssh -o SendEnv=REMOTE_DIR -o StrictHostKeyChecking=no -vvv -T -i ~/{slave_keypair_name} ${ami[1]}@$SERVER_IP <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
+ssh -o SendEnv=REMOTE_DIR -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair_name} ${ami[1]}@$SERVER_IP <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
   	ssh-keyscan -H -t rsa $CLIENT_IP  >> ~/.ssh/known_hosts
   	# Pulls the libfabric repository and checks out the pull request commit
 	echo "==> Building libfabric"
