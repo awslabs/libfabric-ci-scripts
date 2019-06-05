a93d835faa60f2729b4e9ec6cd412e49833a5df3
dkothar <dkothar@amazon.com>
2019-06-05 13:24:10 +0000

    Testing simple script

--- single-node.sh
+++ single-node.sh
@@ -15,11 +15,11 @@ SERVER_IP=$(aws ec2 describe-instances --instance-ids ${SERVER_ID} --query "Rese
 function ssh_slave_node()
 {
 #ssh -o SendEnv=REMOTE_DIR -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair_name} ${ami[1]}@${SERVER_IP} 'bash -x -s' < ~/libfabric-ci-scripts/install-libfabric.sh <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
- ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair_name} ${ami[1]}@${SERVER_IP} "bash -s" << ~/libfabric-ci-scripts/temp.sh 
+ #ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair_name} ${ami[1]}@${SERVER_IP} "bash -s" << ~/libfabric-ci-scripts/temp.sh 
     #${REMOTE_DIR}/libfabric/fabtests/install/bin/runfabtests.sh -v ${EXCLUDE} ${PROVIDER} 127.0.0.1 127.0.0.1
 #EOF
 }
-
-ssh_slave_node
+ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair_name} ${ami[1]}@${SERVER_IP} "bash -s" < ~/libfabric-ci-scripts/temp.sh
+#ssh_slave_node
 AWS_DEFAULT_REGION=us-west-2 aws ec2 terminate-instances --instance-ids $SERVER_ID
 exit $EXIT_CODE
