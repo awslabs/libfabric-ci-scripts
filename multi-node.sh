eb201086916e033f46c2ff9d4c8c843e1057aaf2
dkothar <dkothar@amazon.com>
2019-06-06 06:09:07 +0000

    Done

--- multi-node.sh
+++ multi-node.sh
@@ -10,34 +10,31 @@ instance_code=1
 iteration=10
 
 # Starts as many Instances as specified in $NODES 
-INSTANCE_IDS=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --count ${NODES}:${NODES} --query "Instances[*].InstanceId"   --output=text)
+INSTANCE_IDS=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair} --security-group-id ${slave_security_group} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --count ${NODES}:${NODES} --query "Instances[*].InstanceId"   --output=text)
 INSTANCE_IDS=($INSTANCE_IDS)
-echo "$INSTANCE_IDS"
+
 # Holds testing every 15 seconds for 40 attempts until the instance status check
 # is ok
 function test_instance_status()
 {
-    echo $1
     aws ec2 wait instance-status-ok --instance-ids $1
 }
 
 # Test connection, SSH into nodes and install libfabric
 function ssh_slave_node() 
 {
-    echo $1
     while [ ${instance_code} -ne 0 ] && [ ${iteration} -ne 0 ]; do
         sleep 5
-        ssh -q -o ConnectTimeout=1 -o StrictHostKeyChecking=no -i ~/${slave_keypair_name} ${ami[1]}@$1 exit
+        ssh -q -o ConnectTimeout=1 -o StrictHostKeyChecking=no -i ~/${slave_keypair} ${ami[1]}@$1 exit
         instance_code=$?
         iteration=${iteration}-1
     done
-    ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair_name} ${ami[1]}@$1 "bash -s" -- < $WORKSPACE/libfabric-ci-scripts/install-libfabric.sh "$REMOTE_DIR" "$PULL_REQUEST_ID" "$PULL_REQUEST_REF" "$PROVIDER"
+    ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair} ${ami[1]}@$1 "bash -s" -- < $WORKSPACE/libfabric-ci-scripts/install-libfabric.sh "$REMOTE_DIR" "$PULL_REQUEST_ID" "$PULL_REQUEST_REF" "$PROVIDER"
 }
 
 # SSH into nodes and install libfabric
 for ID in ${INSTANCE_IDS[@]}
 do
-    echo $ID
     test_instance_status "$ID" & 
 done
 wait
@@ -46,16 +43,14 @@ wait
 INSTANCE_IPS=$(aws ec2 describe-instances --instance-ids ${INSTANCE_IDS} --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text)
 INSTANCE_IPS=($INSTANCE_IPS)
 
-count=0
 for IP in ${INSTANCE_IPS[@]}
 do  
-    echo $IP
     ssh_slave_node "$IP" "count" &
 done
 wait
 
-#SSH into SERVER node and run fabtest. INSTANCE_IP[0] used as server
-ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair_name} ${ami[1]}@${INSTANCE_IPS[0]} <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
+#SSH into SERVER node and run fabtest. INSTANCE_IPS[0] used as server
+ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair} ${ami[1]}@${INSTANCE_IPS[0]} <<-EOF && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
 # Runs all the tests in the fabtests suite while only expanding failed cases
 EXCLUDE=${REMOTE_DIR}/libfabric/fabtests/install/share/fabtests/test_configs/${PROVIDER}/${PROVIDER}.exclude
 echo $EXCLUDE
--- single-node.sh
+++ single-node.sh
@@ -4,7 +4,7 @@ set +x
 slave_name=slave_$label
 slave_value=${!slave_name}
 ami=($slave_value)
-SERVER_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair_name} --security-group-id ${security_id} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId" --output=text)
+SERVER_ID=$(AWS_DEFAULT_REGION=us-west-2 aws ec2 run-instances --tag-specification 'ResourceType=instance,Tags=[{Key=Type,Value=Slave},{Key=Name,Value=Slave}]' --image-id ${ami[0]} --instance-type ${instance_type} --enable-api-termination --key-name ${slave_keypair} --security-group-id ${slave_security_group} --subnet-id ${subnet_id} --placement AvailabilityZone=${availability_zone} --query "Instances[*].InstanceId" --output=text)
 REMOTE_DIR=/home/${ami[1]}
 instance_code=1
 iteration=10
@@ -18,7 +18,7 @@ function test_ssh()
 {
     while [ ${instance_code} -ne 0 ] && [ ${iteration} -ne 0 ]; do
         sleep 5
-        ssh -q -o ConnectTimeout=1 -o StrictHostKeyChecking=no -i ~/${slave_keypair_name} ${ami[1]}@${SERVER_IP} exit
+        ssh -q -o ConnectTimeout=1 -o StrictHostKeyChecking=no -i ~/${slave_keypair} ${ami[1]}@${SERVER_IP} exit
         instance_code=$?
         iteration=${iteration}-1
     done
@@ -27,7 +27,7 @@ function test_ssh()
 # SSH into slave EC2 instance
 function ssh_slave_node() 
 {
-    ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair_name} ${ami[1]}@${SERVER_IP} "bash -s" -- < $WORKSPACE/libfabric-ci-scripts/single-node-install-libfabric.sh "$REMOTE_DIR" "$PULL_REQUEST_ID" "$PULL_REQUEST_REF" "$PROVIDER" && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
+    ssh -o StrictHostKeyChecking=no -vvv -T -i ~/${slave_keypair} ${ami[1]}@${SERVER_IP} "bash -s" -- < $WORKSPACE/libfabric-ci-scripts/single-node-install-libfabric.sh "$REMOTE_DIR" "$PULL_REQUEST_ID" "$PULL_REQUEST_REF" "$PROVIDER" && { echo "Build success" ; EXIT_CODE=0 ; } || { echo "Build failed"; EXIT_CODE=1 ;}
 }
 
 # Creates a script for building libfabric on a single node by appending
