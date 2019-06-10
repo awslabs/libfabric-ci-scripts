e8e2b5c46fcd9f7dedf74cd0068c245a143dfe1b
dkothar <dkothar@amazon.com>
2019-06-10 14:04:28 +0000

    trying

--- multi-node.sh
+++ multi-node.sh
@@ -105,6 +105,7 @@ for i in $(seq 1 $N)
 do
     execute_runfabtest "$i" &
 done
+echo "DONE"
 rm $WORKSPACE/libfabric-ci-scripts/${label}.sh
 # Terminates all slave nodes
 AWS_DEFAULT_REGION=us-west-2 aws ec2 terminate-instances --instance-ids ${INSTANCE_IDS[@]}
