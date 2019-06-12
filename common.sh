fd419df604406c4c3921cbc01bbde2b2231761fe
dkothar <dkothar@amazon.com>
2019-06-12 19:14:55 +0000

    Slave nodes created using AWS CLI commands

--- common.sh
+++ common.sh
@@ -5,9 +5,6 @@ prepare_script()
 {
     set_var
     prepare_${label}
-    if [ ${PROVIDER} = "efa" ]; then
-        efa_kernel_drivers_${label}
-    fi
     cat install-libfabric.sh >> ${label}.sh
 }
 prepare_alinux()
@@ -21,15 +18,11 @@ EOF
 prepare_rhel()
 {
     prepare_alinux
-    cat <<-EOF >>${label}.sh
-    sudo yum -y install wget
-    sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-EOF
 }
 
 prepare_ubuntu()
 {
-    cat <<-"EOF" >> ${label}.sh
+    cat <<-EOF >> ${label}.sh
     sudo apt-get update
     sudo apt -y install python
     sudo apt -y install autoconf
@@ -51,20 +44,6 @@ set_var()
 EOF
 }
 
-efa_kernel_drivers_alinux()
-{
-    
-}
-
-efa_kernel_drivers_rhel()
-{
-
-}
-
-efa_kernel_drivers_ubuntu()
-{
-
-}
 # Poll for the SSH daemon to come up before proceeding. The SSH poll retries 40 times with a 5-second timeout each time,
 # which should be plenty after `instance-status-ok`. SSH into nodes and install libfabric
 test_ssh()
@@ -81,39 +60,6 @@ test_ssh()
         slave_poll_count=$((slave_poll_count+1))
     done
 }
-<<<<<<< HEAD
-=======
 
-efa_kernel_drivers()
-{
-    cat <<-"EOF" >> ${label}.sh
-    wget https://github.com/amzn/amzn-drivers/archive/efa_linux_0.9.2.tar.gz
-    tar zxvf efa_linux_0.9.2.tar.gz
-    cd ${HOME}/amzn-drivers-efa_linux_0.9.2/kernel/linux/efa/
-    sudo make
-    wait
-    sudo insmod efa.ko
-    sudo modprobe ib_core
-    sudo modprobe ib_uverbs
-EOF
-}
-
-ubuntu_kernel_upgrade()
-{
-    test_ssh $1
-    ssh -o StrictHostKeyChecking=no -T -i ~/${slave_keypair} ${ami[1]}@$1 <<-EOF
-    sudo apt-get update
-    sudo apt -y install python
-    sudo apt -y install autoconf
-    sudo apt -y install libltdl-dev
-    echo "Installing kernel updgrad and then rebooting"
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-    wait
-    sudo reboot
-EOF
-}
-
->>>>>>> 5a15584... export not a function
 export -f prepare_script
 export -f test_ssh
-export -f ubuntu_kernel_upgrade
