6cc0f0966320d8325d6b69071db88949a635f5a5
dkothar <dkothar@amazon.com>
2019-06-13 20:32:21 +0000

    Testing gid

--- single-node.sh
+++ single-node.sh
@@ -21,7 +21,8 @@ installation_script
 cat <<-"EOF" >> ${label}.sh
 ssh-keygen -f ${HOME}/.ssh/id_rsa -N "" > /dev/null
 cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
-${HOME}/libfabric/fabtests/install/bin/runfabtests.sh -v ${EXCLUDE} ${PROVIDER} 127.0.0.1 127.0.0.1
+gid=$(cat /sys/class/infiniband/efa_0/ports/1/gids/0)
+${HOME}/libfabric/fabtests/install/bin/runfabtests.sh -t all -C "-P 0" -s $gid ${EXCLUDE} ${PROVIDER} 127.0.0.1 127.0.0.1
 EOF
 
 # Test whether node is ready for SSH connection or not
