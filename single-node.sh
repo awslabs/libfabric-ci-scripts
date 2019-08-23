9f47f73528295ca39ec0d309bb77b24798896702
dkothar <dkothar@amazon.com>
2019-08-23 04:06:24 +0000

    Create XML files for Junit to display
    Split log file into separate xml files for each instance, and the
    logs are sequentially named/arranged based on the command/function run.
    Text files are converted into yaml and then to xml, to get command based
    split of logs. Parsing is done once the script exits
    
    Signed-off-by: dkothar <dkothar@amazon.com>

--- single-node.sh
+++ single-node.sh
@@ -10,9 +10,13 @@ NODES=1
 set +x
 create_instance || { echo "==>Unable to create instance"; exit 1; }
 set -x
+
+execution_seq=$((${execution_seq}+1))
 test_instance_status ${INSTANCE_IDS}
+
 get_instance_ip
 
+execution_seq=$((${execution_seq}+1))
 # Kernel upgrade only for Ubuntu and provider EFA
 check_provider_os ${INSTANCE_IPS}
 
@@ -25,21 +29,23 @@ ssh-keygen -f ${HOME}/.ssh/id_rsa -N "" > /dev/null
 cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
 if [ ${PROVIDER} == "efa" ];then
     gid=$(cat /sys/class/infiniband/efa_0/ports/1/gids/0)
-    ${HOME}/libfabric/fabtests/install/bin/runfabtests.sh -v -t all -C "-P 0" -s $gid -c $gid ${EXCLUDE} ${PROVIDER} 127.0.0.1 127.0.0.1
+    ${HOME}/libfabric/fabtests/install/bin/runfabtests.sh -vvv -t all -C "-P 0" -s $gid -c $gid ${EXCLUDE} ${PROVIDER} 127.0.0.1 127.0.0.1
 else
-    ${HOME}/libfabric/fabtests/install/bin/runfabtests.sh -v ${EXCLUDE} ${PROVIDER} 127.0.0.1 127.0.0.1
+    ${HOME}/libfabric/fabtests/install/bin/runfabtests.sh -vvv ${EXCLUDE} ${PROVIDER} 127.0.0.1 127.0.0.1
 fi
 EOF
 
 # Test whether node is ready for SSH connection or not
 test_ssh ${INSTANCE_IPS}
 
+execution_seq=$((${execution_seq}+1))
 # For single node, the ssh connection is established only once. The script
 # builds libfabric and also executes fabtests
 set +x
 ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no -T -i ~/${slave_keypair} ${ami[1]}@${INSTANCE_IPS} \
     "bash -s" -- <$WORKSPACE/libfabric-ci-scripts/${label}.sh \
-    "$PULL_REQUEST_ID" "$PULL_REQUEST_REF" "$PROVIDER" 2>&1 | tr \\r \\n | sed 's/\(.*\)/'${INSTANCE_IPS}' \1/'
+    "$PULL_REQUEST_ID" "$PULL_REQUEST_REF" "$PROVIDER" 2>&1 | tr \\r \\n | \
+    sed 's/\(.*\)/'${INSTANCE_IPS}' \1/' | tee $WORKSPACE/libfabric-ci-scripts/temp_execute_runfabtests.txt
 EXIT_CODE=${PIPESTATUS[0]}
 set -x
 
