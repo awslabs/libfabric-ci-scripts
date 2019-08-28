e9cfa201812df7524f4964e475af79f0cb77dc7a
Robert Wespetal <wesper@amazon.com>
2019-08-28 16:46:48 -0700

    Split scripts for fabtests and libfabric install
    Signed-off-by: Robert Wespetal <wesper@amazon.com>

---
+++ install-fabtests.sh
@@ -0,0 +1,23 @@
+echo "==> Building fabtests"
+cd ${HOME}
+if [ ! -d libfabric ]; then
+    git clone https://github.com/ofiwg/libfabric
+fi
+cd ${HOME}/libfabric/fabtests
+./autogen.sh
+./configure --with-libfabric=${HOME}/libfabric/install/ \
+    --prefix=${HOME}/libfabric/fabtests/install/ \
+    --enable-debug
+make -j 4
+make install
+
+# Runs all the tests in the fabtests suite between two nodes while only expanding failed cases
+EXCLUDE=${HOME}/libfabric/fabtests/install/share/fabtests/test_configs/${PROVIDER}/${PROVIDER}.exclude
+if [ -f ${EXCLUDE} ]; then
+    EXCLUDE="-R -f ${EXCLUDE}"
+else
+    EXCLUDE=""
+fi
+export LD_LIBRARY_PATH=${HOME}/libfabric/install/lib/:$LD_LIBRARY_PATH >> ~/.bash_profile
+export BIN_PATH=${HOME}/libfabric/fabtests/install/bin/ >> ~/.bash_profile
+export PATH=${HOME}/libfabric/fabtests/install/bin:$PATH >> ~/.bash_profile
--- install-libfabric.sh
+++ install-libfabric.sh
@@ -15,22 +15,3 @@ git checkout $PULL_REQUEST_REF -b PRBranch
     --disable-verbs
 make -j 4
 make install
-echo "==> Building fabtests"
-cd ${HOME}/libfabric/fabtests
-./autogen.sh
-./configure --with-libfabric=${HOME}/libfabric/install/ \
-    --prefix=${HOME}/libfabric/fabtests/install/ \
-    --enable-debug
-make -j 4
-make install
-
-# Runs all the tests in the fabtests suite between two nodes while only expanding failed cases
-EXCLUDE=${HOME}/libfabric/fabtests/install/share/fabtests/test_configs/${PROVIDER}/${PROVIDER}.exclude
-if [ -f ${EXCLUDE} ]; then
-    EXCLUDE="-R -f ${EXCLUDE}"
-else
-    EXCLUDE=""
-fi
-export LD_LIBRARY_PATH=${HOME}/libfabric/install/lib/:$LD_LIBRARY_PATH >> ~/.bash_profile
-export BIN_PATH=${HOME}/libfabric/fabtests/install/bin/ >> ~/.bash_profile
-export PATH=${HOME}/libfabric/fabtests/install/bin:$PATH >> ~/.bash_profile
