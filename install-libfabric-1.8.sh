67683ecea7979c5de1f3e10fa36980af4df79c9f
Wei Zhang <wzam@amazon.com>
2020-01-28 16:44:41 +0000

    Special treatment of libfabric v1.8.x branch
    The Latest EFA Installer installs the Open MPI built with libfabric
    1.9, which will not work with the v1.8.x branch.
    
    This patch check the pull request's source code version, and use
    EFA Installer 1.7.1 for if the source code is 1.8.x, and also will
    not build rdma-core v27 for it
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

---
+++ install-libfabric-1.8.sh
@@ -0,0 +1,20 @@
+echo "==> Building libfabric 1.8.x"
+cd ${HOME}
+git clone https://github.com/ofiwg/libfabric
+cd ${HOME}/libfabric
+git fetch origin +refs/pull/$PULL_REQUEST_ID/*:refs/remotes/origin/pr/$PULL_REQUEST_ID/*
+git checkout $PULL_REQUEST_REF -b PRBranch
+./autogen.sh
+./configure --prefix=${HOME}/libfabric/install/ \
+    --enable-debug  \
+    --enable-mrail  \
+    --enable-tcp    \
+    --enable-rxm    \
+    --disable-rxd   \
+    --disable-verbs
+make -j 4
+make install
+LIBFABRIC_INSTALL_PATH=${HOME}/libfabric/install
+# ld.so.conf.d files are preferred in alphabetical order
+# this doesn't seem to be working for non-interactive shells
+sudo bash -c "echo ${LIBFABRIC_INSTALL_PATH} > /etc/ld.so.conf.d/aaaa-libfabric-testing.sh"
