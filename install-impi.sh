b6e68851725be37247efb969d8e66e47643b7a7e
Robert Wespetal <wesper@amazon.com>
2020-04-02 21:45:09 -0700

    Switch to Intel MPI 2019 U7

--- install-impi.sh
+++ install-impi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-curl -O http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16120/l_mpi_2019.6.166.tgz
-tar -zxf l_mpi_2019.6.166.tgz
-cd l_mpi_2019.6.166
+curl -O http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16546/l_mpi_2019.7.217.tgz
+tar -zxf l_mpi_2019.7.217.tgz
+cd l_mpi_2019.7.217
 sed -e "s/decline/accept/" silent.cfg > accept.cfg
 sudo ./install.sh -s accept.cfg
