77f0b2e1f1b258ab993ec77913c327e239399bf6
Shi Jin <sjina@amazon.com>
2020-04-18 20:02:36 -0700

    Bug fix: add missing $libfabric_job_type parameter when executing mpi_ring_c_test.sh and mpi_osu_test.sh scripts in multi-node-efa-minimal.sh
    SIM: https://issues.amazon.com/issues/EFA-1159
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- multi-node-efa-minimal.sh
+++ multi-node-efa-minimal.sh
@@ -135,7 +135,7 @@ test_list="impi"
 for mpi in $test_list; do
     execution_seq=$((${execution_seq}+1))
     ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no -T -i ~/${slave_keypair} ${ami[1]}@${INSTANCE_IPS[0]} \
-        bash mpi_ring_c_test.sh ${mpi} ${INSTANCE_IPS[@]} | tee ${output_dir}/temp_execute_ring_c_efa_minimal_${mpi}.txt
+        bash mpi_ring_c_test.sh ${mpi} ${libfabric_job_type} ${INSTANCE_IPS[@]} | tee ${output_dir}/temp_execute_ring_c_efa_minimal_${mpi}.txt
 
     set +e
     grep -q "Test Passed" ${output_dir}/temp_execute_ring_c_efa_minimal_${mpi}.txt
@@ -146,7 +146,7 @@ for mpi in $test_list; do
     set -e
 
     ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no -T -i ~/${slave_keypair} ${ami[1]}@${INSTANCE_IPS[0]} \
-        bash mpi_osu_test.sh ${mpi} ${INSTANCE_IPS[@]} | tee ${output_dir}/temp_execute_osu_efa_minimal_${mpi}.txt
+        bash mpi_osu_test.sh ${mpi} ${libfabric_job_type} ${INSTANCE_IPS[@]} | tee ${output_dir}/temp_execute_osu_efa_minimal_${mpi}.txt
 
     set +e
     grep -q "Test Passed" ${output_dir}/temp_execute_osu_efa_minimal_${mpi}.txt
