a5da9c06faa8a5f9c52972a658dbb2237cf1aba7
Dipti Kothari <dkothar@amazon.com>
2020-05-05 09:46:20 +0000

    Use S3 mirror for downloading intel mpi U7
    Signed-off-by: Dipti Kothari <dkothar@amazon.com>

--- install-impi.sh
+++ install-impi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --retry 5 -O http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16546/l_mpi_2019.7.217.tgz
+curl --retry 5 -O https://s3.us-west-2.amazonaws.com/subspace-intelmpi/l_mpi_2019.7.217.tgz
 tar -zxf l_mpi_2019.7.217.tgz
 cd l_mpi_2019.7.217
 sed -e "s/decline/accept/" silent.cfg > accept.cfg
