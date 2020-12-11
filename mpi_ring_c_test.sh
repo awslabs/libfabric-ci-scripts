1e52cc488788572dd6cfc75378e2638b38a81a4a
William Zhang <wilzhang@amazon.com>
2020-12-11 11:29:30 -0800

    Add a fortran ring compile and run test to mpi_ring_c_test.sh
    Signed-off-by: William Zhang <wilzhang@amazon.com>

--- mpi_ring_c_test.sh
+++ mpi_ring_c_test.sh
@@ -11,9 +11,11 @@ libfabric_job_type=$1
 shift
 hosts=$@
 hostfile=$(mktemp)
-out=$(mktemp)
+out1=$(mktemp)
+out2=$(mktemp)
 
 curl ${CURL_OPT} -O https://raw.githubusercontent.com/open-mpi/ompi/master/examples/ring_c.c
+curl ${CURL_OPT} -O https://raw.githubusercontent.com/open-mpi/ompi/master/examples/ring_usempi.f90
 
 if [ "${mpi}" == "ompi" ]; then
     ompi_setup
@@ -26,21 +28,32 @@ fi
 
 host_setup ${hostfile} ${hosts}
 mpicc -o /tmp/ring_c ring_c.c
+# Fortran compile test
+mpif90 -o /tmp/ring_fortran ring_usempi.f90
 for host in $hosts; do
     scp /tmp/ring_c $host:/tmp
+    scp /tmp/ring_fortran $host:/tmp
 done
 
 $mpirun_cmd --version
-$mpirun_cmd -n $(( $ranks * $# )) -hostfile $hostfile /tmp/ring_c 2>&1 | tee $out
-if [ $? -ne 0 ] || ! grep -q "Process 0 exiting" $out; then
+$mpirun_cmd -n $(( $ranks * $# )) -hostfile $hostfile /tmp/ring_c 2>&1 | tee $out1
+if [ $? -ne 0 ] || ! grep -q "Process 0 exiting" $out1; then
     echo "mpirun ring_c failed"
     exit 1
 fi
 
+$mpirun_cmd -n $(( $ranks * $# )) -hostfile $hostfile /tmp/ring_fortran 2>&1 | tee $out2
+if [ $? -ne 0 ] || ! grep -q "Process  0 exiting" $out2; then
+    echo "mpirun ring_fortran (f90) failed"
+    exit 1
+fi
+
 if [ "${mpi}" == "ompi" ]; then
-    check_efa_ompi $out
+    check_efa_ompi $out1
+    check_efa_ompi $out2
 elif [ "${mpi}" == "impi" ]; then
-    check_efa_impi $out
+    check_efa_impi $out1
+    check_efa_impi $out2
 fi
 
 echo "Test Passed"
