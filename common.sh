70585ccad22fd4d6ab72a2270edfdbbd6344f876
Shi Jin <sjina@amazon.com>
2021-01-22 10:06:19 -0800

    Rename TEST_GDR flag to BUILD_GDR.
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- common.sh
+++ common.sh
@@ -15,7 +15,7 @@ fi
 RUN_IMPI_TESTS=${RUN_IMPI_TESTS:-1}
 ENABLE_PLACEMENT_GROUP=${ENABLE_PLACEMENT_GROUP:-0}
 TEST_SKIP_KMOD=${TEST_SKIP_KMOD:-0}
-TEST_GDR=${TEST_GDR:-0}
+BUILD_GDR=${BUILD_GDR:-0}
 
 get_opensuse1502_ami_id() {
     region=$1
@@ -262,7 +262,7 @@ create_instance()
     )
     create_instance_count=0
     error=1
-    if [ $ami_arch = "x86_64" ] && [ $TEST_GDR -eq 0 ]; then
+    if [ $ami_arch = "x86_64" ] && [ $BUILD_GDR -eq 0 ]; then
         case "${PROVIDER}" in
             efa)
                 instance_type=c5n.18xlarge
@@ -278,7 +278,7 @@ create_instance()
             *)
                 exit 1
         esac
-    elif [ $TEST_GDR -eq 1 ]; then
+    elif [ $BUILD_GDR -eq 1 ]; then
         instance_type=g4dn.metal
         network_interface="[{\"DeviceIndex\":0,\"DeleteOnTermination\":true,\"InterfaceType\":\"efa\",\"Groups\":[\"${slave_security_group}\"]"
     else
@@ -569,7 +569,7 @@ EOF
     fi
     if [ $TEST_SKIP_KMOD -eq 1 ]; then
         echo "sudo ./efa_installer.sh -y -k" >> ${tmp_script}
-    elif [ $TEST_GDR -eq 1 ]; then
+    elif [ $BUILD_GDR -eq 1 ]; then
         echo "sudo ./efa_installer.sh -y -g" >> ${tmp_script}
     else
         echo "sudo ./efa_installer.sh -y" >> ${tmp_script}
