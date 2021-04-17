594c542785237c7a2b01828894c146cf7175a739
Wei Zhang <wzam@amazon.com>
2021-04-17 20:30:40 +0000

    install-nccl.sh: install g++
    g++ is required to compile nccl. This patch installs it.
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- install-nccl.sh
+++ install-nccl.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+
+os_name="$(. /etc/os-release; echo $NAME)"
+if [  "$os_name" == "Ubuntu" ]; then
+    sudo apt-get install -y "g++"
+elif [ "$os_name" == "openSUSE Leap" ] || [ "$os_name" == "SLES" ]; then
+    sudo zypper install -y "gcc-c++"
+else
+    sudo yum install -y "gcc-c++"
+fi
+
 NCCL_VERSION="v2.8.3-1"
 cd $HOME
 git clone -b ${NCCL_VERSION} https://github.com/NVIDIA/nccl.git
 pushd nccl
 make -j src.build CUDA_HOME=/usr/local/cuda NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80"
 if [ $? -ne 0 ]; then
-	echo "NCCL build failed!"
-	exit -1
+    echo "NCCL build failed!"
+    exit -1
 fi
 popd
 
