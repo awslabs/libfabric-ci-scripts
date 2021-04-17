be764c138118b2a088e12cf935112e60b1d9a40a
Wei Zhang <wzam@amazon.com>
2021-04-17 16:57:58 +0000

    install-omb.sh: install g++
    g++ is required to compile OMB, this patch add the installation of
    it.
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- install-omb.sh
+++ install-omb.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+os_name="$(. /etc/os-release; echo $NAME)"
+if [  "$os_name" == "Ubuntu" ]; then
+    sudo apt-get install -y "g++"
+elif [ "$os_name" == "openSUSE Leap" ] || [ "$os_name" == "SLES" ]; then
+    sudo zypper install -y "gcc-c++"
+else
+    sudo yum install -y "gcc-c++"
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install g++, which is required to compile omb"
+    exit -1
+fi
+
 cd $HOME
 source ~/wget_check.sh
 wget_check http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-5.6.2.tar.gz osu-micro-benchmarks-5.6.2.tar.gz
