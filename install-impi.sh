26ca04b7587b0df4367e37435a3c2ad9f992b9ed
Shi Jin <sjina@amazon.com>
2021-08-04 09:51:03 -0700

    Remove the stale install-impi.sh script.
    Intel MPI is installed in the pre-built AMI that the test is running with,
    this script is not used during test and should be removed.
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- install-impi.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source ~/wget_check.sh
-wget_check "https://s3.us-west-2.amazonaws.com/subspace-intelmpi/l_mpi_2019.10.317.tgz" "l_mpi_2019.10.317.tgz"
-tar -zxf l_mpi_2019.10.317.tgz
-cd l_mpi_2019.10.317
-sed -e "s/decline/accept/" silent.cfg > accept.cfg
-sudo ./install.sh -s accept.cfg
